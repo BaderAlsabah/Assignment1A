c5adcc703c7f629d488a95984f767e4271a6d048
Bader Alsabah <Alsabah.dev@gmail.com>
2020-05-04 16:02:01 +0300

    Latest Update

--- Assignment1/ContentView.swift
+++ Assignment1/ContentView.swift
@@ -10,7 +10,105 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            Image("Night")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                VStack{
+                Text("Mishref").foregroundColor(.white).font(.largeTitle)
+                Text("Mostly Clear").foregroundColor(.white)
+                    Text("22°").foregroundColor(.white).font(.system(size: 70, weight: .thin))
+                }.offset(y: -30)
+                HStack{
+                    Text("Tuesday").foregroundColor(.white).bold()
+                    Text("TODAY").foregroundColor(.white)
+                    Spacer()
+                    HStack{
+                        Text("31").foregroundColor(.white).padding(.leading)
+                        Text("24").foregroundColor(.white).padding(.horizontal)
+                    }
+                }.padding(.horizontal).offset(y: 10)
+                ScrollView(.horizontal, showsIndicators: false){
+                VStack{
+                HStack( spacing: 25){
+                    Text("Now").foregroundColor(.white)
+                    Text("5AM").foregroundColor(.white)
+                    Text("6AM").foregroundColor(.white)
+                    Text("7AM").foregroundColor(.white)
+                    Text("8AM").foregroundColor(.white)
+                    Text("10AM").foregroundColor(.white)
+                    Text("12PM").foregroundColor(.white)
+                    Text("14PM").foregroundColor(.white)
+                }
+                HStack( spacing: 38){
+                    Image("Moon").resizable().foregroundColor(.white).frame(width: 25,height: 25)
+                    Image("Moon").resizable().foregroundColor(.white).frame(width: 25,height: 25)
+                    Image("Sunrise").resizable().foregroundColor(.white).frame(width: 25,height: 25)
+                    Image("Cloud").resizable().foregroundColor(.white).frame(width: 25,height: 25)
+                    Image("Moon").resizable().foregroundColor(.white).frame(width: 25,height: 25)
+                    Image("Moon").resizable().foregroundColor(.white).frame(width: 25,height: 25)
+                    Image("Moon").resizable().foregroundColor(.white).frame(width: 25,height: 25)
+                    Image("Moon").resizable().foregroundColor(.white).frame(width: 25,height: 25)
+                }
+                HStack( spacing: 36){
+                    Text("22°").foregroundColor(.white)
+                    Text("22°").foregroundColor(.white)
+                    Text("22°").foregroundColor(.white)
+                    Text("22°").foregroundColor(.white)
+                    Text("22°").foregroundColor(.white)
+                    Text("22°").foregroundColor(.white)
+                    Text("22°").foregroundColor(.white)
+                    Text("22°").foregroundColor(.white)
+                }
+                    }}.padding()
+                VStack(spacing: 10){
+                    HStack( spacing: 41){
+                        Text("Wednesday").foregroundColor(.white)
+                        Image("Cloud").resizable().foregroundColor(.white).frame(width: 30,height: 30).offset(x: 25)
+                        Spacer()
+                        Text("29").foregroundColor(.white).offset(x:20)
+                        Text("24").foregroundColor(.white)
+                    }.padding(.horizontal)
+                HStack( spacing: 41){
+                    Text("Wednesday").foregroundColor(.white)
+                    Image("Cloud").resizable().foregroundColor(.white).frame(width: 30,height: 30).offset(x: 25)
+                    Spacer()
+                    Text("29").foregroundColor(.white).offset(x:20)
+                    Text("24").foregroundColor(.white)
+                }.padding(.horizontal)
+                HStack( spacing: 41){
+                    Text("Wednesday").foregroundColor(.white)
+                     Image("Cloud").resizable().foregroundColor(.white).frame(width: 30,height: 30).offset(x: 25)
+                    Spacer()
+                    Text("29").foregroundColor(.white).offset(x:20)
+                    Text("24").foregroundColor(.white)
+                }.padding(.horizontal)
+                    HStack( spacing: 41){
+                        Text("Wednesday").foregroundColor(.white)
+                         Image("Cloud").resizable().foregroundColor(.white).frame(width: 30,height: 30).offset(x: 25)
+                        Spacer()
+                        Text("29").foregroundColor(.white).offset(x:20)
+                        Text("24").foregroundColor(.white)
+                    }.padding(.horizontal)
+                HStack( spacing: 41){
+                    Text("Wednesday").foregroundColor(.white)
+                     Image("Cloud").resizable().foregroundColor(.white).frame(width: 30,height: 30).offset(x: 25)
+                    Spacer()
+                    Text("29").foregroundColor(.white).offset(x:20)
+                    Text("24").foregroundColor(.white)
+                }.padding(.horizontal)
+                HStack( spacing: 41){
+                    Text("Wednesday").foregroundColor(.white)
+                     Image("Cloud").resizable().foregroundColor(.white).frame(width: 30,height: 30).offset(x: 25)
+                    Spacer()
+                    Text("29").foregroundColor(.white).offset(x:20)
+                    Text("24").foregroundColor(.white)
+                }.padding(.horizontal)
+                }.offset(y:40)
+            
+            }
+        }
     }
 }
 
